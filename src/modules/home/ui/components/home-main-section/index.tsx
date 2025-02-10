ce4ae3cc9c532efc5a0ac22b9c529104a4a8ba39
RISHII7 <rishikesh.palande07@gmail.com>
2025-02-10 21:24:39 +0530

    Basic Layout

---
+++ src/modules/home/ui/components/home-main-section/index.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+
+import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+    {
+        title: "Home",
+        url: "/",
+        icon: HomeIcon
+    },
+    {
+        title: "Subscriptions",
+        url: "/feed/subscriptions",
+        icon: PlaySquareIcon,
+        auth: true
+    },
+    {
+        title: "Trending",
+        url: "/feed/trending",
+        icon: FlameIcon
+    }
+];
+
+export const MainSection = () => {
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item, index) => (
+                        <SidebarMenuItem key={index}>
+                            <SidebarMenuButton
+                                tooltip={item.title}
+                                asChild
+                                isActive={false}    //TODO: Change to look at current pathname
+                                onClick={() => { }} //TODO: Do something on click
+                            >
+                                <Link href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
+};
